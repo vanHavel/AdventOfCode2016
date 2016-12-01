9b6379339467d41250905c05d7f175765eded504
Lukas Huwald <lukashuwald@online.de>
2016-12-01 21:07:03 +0100

    simplified firstRepetition

--- utility/lists.hs
+++ utility/lists.hs
@@ -2,8 +2,6 @@ module Utility.Lists where
     
 -- get first element of list appearing twice, if existing
 firstRepetition :: Eq a => [a] -> Maybe a
-firstRepetition xs = frs xs [] where
-    frs :: Eq a => [a] -> [a] -> Maybe a
-    frs [] _ = Nothing
-    frs (x:xs) storage | elem x storage = Just x
-                       | otherwise      = frs xs (x:storage)
+firstRepetition [] = Nothing
+firstRepetition (x:xs) | elem x xs = Just x
+                       | otherwise = firstRepetition xs 
